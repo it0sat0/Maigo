ab9c9af6cb70e187b6f8a2f87f40c0f5887e068c
Olivia <wild_hosikuzu@icloud.com>
2018-03-09 01:56:31 +0900

    first commit

---
+++ map.js
@@ -0,0 +1,13 @@
+﻿function initMap() {
+	var px = 35.1239654;
+	var py = 136.9417741;
+	var opts = {
+		zoom: 15,
+		center: new google.maps.LatLng(px, py)
+	};
+	var map = new google.maps.Map(document.getElementById("map"), opts);
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(px, py),
+        map: map
+     });
+}
